eb00925a4573670c11738a68a20240a4694ece05
Aditya Saxena <adity09@outlook.com>
2025-10-31 23:36:25 +0530

    Initial Commit

---
+++ MBinge/MBingeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBingeApp.swift
+//  MBinge
+//
+//  Created by Aditya Saxena on 31/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MBingeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
